b89276f6a3b2baf1270048a59cec761702275861
Ayman Elomari <aelomari@Aymans-MacBook-Pro.local>
2024-02-14 12:39:40 +0100

    done

--- srcs/check_args.c
+++ srcs/check_args.c
@@ -6,93 +6,9 @@
 /*   By: aelomari <aelomari@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 15:53:21 by aelomari          #+#    #+#             */
-/*   Updated: 2024/02/14 12:02:52 by aelomari         ###   ########.fr       */
+/*   Updated: 2024/02/14 12:39:08 by aelomari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	ft_error(void)
-{
-	write(2, "Error\n", 6);
-	system("leaks push_swap");
-	exit(1);
-}
-
-static int	isduplicate(long num, char **args, int i)
-{
-	i++;
-	while (args[i])
-	{
-		if (ft_atoi(args[i]) == num)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-static int	isnumber(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (str[i] == '-' || str[i] == '+')
-		i++;
-	while (str[i])
-	{
-		if (!ft_isdigit(str[i]))
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-char	**check_arg(int ac, char **av)
-{
-	char	**args;
-	long	tmp;
-	int		i;
-
-	if (ac < 2)
-		exit(1);
-	else if (ac == 2)
-		args = ft_split(av[1], ' ');
-	else
-		args = av;
-	i = 1;
-	while (args[i])
-	{
-		tmp = ft_atoi(args[i]);
-		if (!isnumber(args[i]) || isduplicate(tmp, args, i))
-			ft_error();
-		else if (tmp > INT_MAX || tmp < INT_MIN)
-			ft_error();
-		i++;
-	}
-	return (args);
-}
-long ft_atoi(const char *str)
-{
-	long long int		i;
-	long long int		sign;
-	long long int		res;
-
-	i = 0;
-	sign = 1;
-	res = 0;
-	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
-		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign = -1;
-		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		res = res * 10 + (str[i] - '0');
-		i++;
-	}
-	return (res * sign);
-}
--- srcs/stack_init.c
+++ srcs/stack_init.c
@@ -3,35 +3,11 @@
 /*                                                        :::      ::::::::   */
 /*   stack_init.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aelomari <aelomari@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aelomari <aelomari@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 16:49:34 by aelomari          #+#    #+#             */
-/*   Updated: 2024/01/30 17:40:09 by aelomari         ###   ########.fr       */
+/*   Updated: 2024/02/14 12:38:59 by aelomari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-
-t_stack	*stack_init(t_stack **stack, char **av)
-{
-	int		i;
-	t_stack	*head;
-	t_stack	*nodes;
-
-	nodes = NULL;
-	head = NULL;
-	i = 1;
-	while (av[i])
-		i++;
-	i--;
-	while (i > 0)
-	{
-		nodes = ft_stacknew(ft_atoi(av[i]));
-		if (!head)
-			head = nodes;
-		else
-			ft_stackadd_back(&head, nodes);
-		i--;
-	}
-	return (head);
-}
