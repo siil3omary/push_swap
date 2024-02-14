8a4f978e1e23b454e9805d69e16d1565c4cb77d5
Mona Lisa <l3omary@icloud.com>
2024-02-14 14:28:04 +0100

    Done

--- main.c
+++ main.c
@@ -6,14 +6,26 @@
 /*   By: aelomari <aelomari@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 11:46:46 by aelomari          #+#    #+#             */
-/*   Updated: 2024/02/14 12:38:47 by aelomari         ###   ########.fr       */
+/*   Updated: 2024/02/14 14:25:19 by aelomari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/push_swap.h"
 
+void check_args(int ac, char** av){
+char **avs;
+int i = 1;
+
+while (i <  ac )
+{
+	avs = ft_split(av[i] , ' ');
+	i++;
+}
+
+	
+}
 
 int	main(int ac, char **av)
 {
-
+	
 }
